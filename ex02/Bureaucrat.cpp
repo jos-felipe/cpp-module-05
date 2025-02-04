098ed12e676b411c9d7647b2487d697ec0c41ac4
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2025-02-04 17:39:22 -0300

    implementing the executeForm()

--- ex02/Bureaucrat.cpp
+++ ex02/Bureaucrat.cpp
@@ -6,12 +6,12 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 10:08:29 by josfelip          #+#    #+#             */
-/*   Updated: 2025/02/04 13:07:49 by josfelip         ###   ########.fr       */
+/*   Updated: 2025/02/04 17:37:18 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AForm.hpp"
 #include "Bureaucrat.hpp"
-#include "Form.hpp"
 
 const char *Bureaucrat::GradeTooHighException::what() const throw()
 {
@@ -76,23 +76,38 @@ void Bureaucrat::decrementGrade()
 	_grade++;
 }
 
-void Bureaucrat::signForm(Form &form)
+void Bureaucrat::signForm(AForm &form)
 {
-    try
-    {
-        form.beSigned(*this);
-        std::cout << this->_name << " signed " << form.getName() << std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cout << this->_name << " couldn't sign " << form.getName() 
-            << " because " << e.what() << std::endl;
-    }
+	try
+	{
+		form.beSigned(*this);
+		std::cout << this->_name << " signed " << form.getName() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << this->_name << " couldn't sign " << form.getName() << 
+		" because " << e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::executeForm(AForm const &form) const
+{
+	try
+	{
+		form.execute(*this);
+
+		std::cout << this->_name << " executed " << form.getName() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << this->_name << " couldn't execute " << form.getName() << 
+		" because " << e.what() << std::endl;
+	}
 }
 
 std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
 {
-	out << bureaucrat.getName() << ", bureaucrat grade " 
-	<< bureaucrat.getGrade();
+	out << bureaucrat.getName() << 
+	", bureaucrat grade " << bureaucrat.getGrade();
 	return (out);
 }
