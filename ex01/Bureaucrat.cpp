1fab879591a83b46a23778401cfe0a7b13277a93
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2025-02-04 13:09:27 -0300

    Add signForm method to Bureaucrat class with exception handling for form signing

--- ex01/Bureaucrat.cpp
+++ ex01/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 10:08:29 by josfelip          #+#    #+#             */
-/*   Updated: 2025/02/04 12:13:10 by josfelip         ###   ########.fr       */
+/*   Updated: 2025/02/04 13:07:49 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,20 @@ void Bureaucrat::decrementGrade()
 	if (_grade + 1 > 150)
 		throw Bureaucrat::GradeTooLowException();
 	_grade++;
+}
+
+void Bureaucrat::signForm(Form &form)
+{
+    try
+    {
+        form.beSigned(*this);
+        std::cout << this->_name << " signed " << form.getName() << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << this->_name << " couldn't sign " << form.getName() 
+            << " because " << e.what() << std::endl;
+    }
 }
 
 std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
